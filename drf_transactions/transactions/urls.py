c90b48dc6f947ca563afabc8dde25b3f7394ebc2
bowler <m.bowler@f5.com>
2023-07-31 20:40:26 -0500

    additional url pattern support

--- drf_transactions/transactions/urls.py
+++ drf_transactions/transactions/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from transactions import views
 
 urlpatterns = [
+    path('transactions', views.transaction_list),
     path('transactions/', views.transaction_list),
+    path('transactions/<int:pk>', views.transaction_detail),
     path('transactions/<int:pk>/', views.transaction_detail),
 ]
